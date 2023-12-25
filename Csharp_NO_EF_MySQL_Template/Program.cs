d385b4979d5835a113eecc0cd1bbd13cf85b9c79
weber.hsu53 <weber.hsu@titansoft.com.sg>
2023-12-25 20:36:15 +0800

    Success connect to DB

--- Csharp_NO_EF_MySQL_Template/Program.cs
+++ Csharp_NO_EF_MySQL_Template/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using MySql.Data.MySqlClient;
+
+string connectionString = "Server=localhost;Port=3306;Database=testing;User ID=root;Password=example;SslMode=None;";
+using (MySqlConnection connection = new MySqlConnection(connectionString))
+{
+    try
+    {
+        connection.Open();
+        string query = "SELECT * FROM testing1";
+        using (MySqlCommand command = new MySqlCommand(query, connection))
+        {
+            var result =await command.ExecuteReaderAsync();
+            Console.WriteLine($"Query Result: {result}");
+        }
+        Console.WriteLine("Connected to the database!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
